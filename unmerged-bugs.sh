8c6a780c546be251e76ebb519c2939d6f72b5483
Adam Dymitruk <adamd@activestate.com>
2015-04-29 11:54:34 -0700

    need to recurse the submodules

--- unmerged-bugs.sh
+++ unmerged-bugs.sh
@@ -2,6 +2,6 @@
 cd stackato-release
 git checkout master --quiet
 git pull --quiet
-git submodule update --init --quiet
+git submodule update --init --recursive --quiet
 git branch -r --no-merged | grep "[0-9]\{6\}" | while read branch; do echo "$(git log -1 --format=%an $branch)" "$branch"; done | sort | grep -v Entering | sed -e 's/.*\([0-9]\{6\}\).*/\1/' | egrep '^3'
 git submodule foreach --recursive 'git branch -r --no-merged | grep "[0-9]\{6\}" | while read branch; do echo "$(git log -1 --format=%an $branch)" "$branch"; done | sort' | grep -v Entering | sed -e 's/.*\([0-9]\{6\}\).*/\1/' | egrep '^3'
